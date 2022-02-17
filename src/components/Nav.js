4639c5d94469da155f4ddf69b82cb3cba92b569f
Tina Stanczyk <t9.stanczyk@gmail.com>
2022-02-17 14:48:58 -0500

    changed 'work' tab to 'portfolio'

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,13 +8,13 @@ function Nav() {
       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
         <ul className = 'right hide-on-med-and-down'>
           <li><a href='#'>About</a></li>
-          <li><a href='#'>Work</a></li>
+          <li><a href='#'>Portfolio</a></li>
           <li><a href='#'>Resume</a></li>
           <li><a href='#'>Contact</a></li>
         </ul>
         <ul className='sidenav' id="mobile-demo">
         <li><a href='#'>About</a></li>
-          <li><a href='#'>Work</a></li>
+          <li><a href='#'>Portfolio</a></li>
           <li><a href='#'>Resume</a></li>
           <li><a href='#'>Contact</a></li>
         </ul>
