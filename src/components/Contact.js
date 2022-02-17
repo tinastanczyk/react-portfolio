74efc6cc2d3cd4819e4a13efcc294729c02446bd
Tina Stanczyk <t9.stanczyk@gmail.com>
2022-02-17 18:25:10 -0500

    created contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
-import { checkName, validateEmail, checkMessage } from '../utils/helpers';
+// import { checkName, validateEmail, checkMessage } from '../utils/helpers';
 
 function Form() {
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
@@ -9,9 +9,15 @@ function Form() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("contactName")} />
-      <input {...register("email")} />
-      <input {...register("message")} />
+      <input placeholder={'Name'} {...register("contactName", {required: true, maxLength: 100})} />
+      {errors.contactName?.type === 'required' && 'Name is required.'}
+      <input placeholder={'Email'} {...register("email", {required: true, pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/})} />
+      {errors.email?.type === 'required' && 'Email is required.'}
+      {errors.email?.type === 'pattern' && 'Email must be in proper format. For example: first.last@domain.com.'}
+      <input placeholder={'Message'} {...register("message", {required: true, maxLength: 1000})} />
+      {errors.message?.type === 'required' && 'A message is required.'}
+      <br />
+      <br />
       <input type='submit' />
     </form>
   );
