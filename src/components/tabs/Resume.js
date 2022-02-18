79d0f084c9d91c7c9f3247a955783796519803a1
Tina Stanczyk <t9.stanczyk@gmail.com>
2022-02-17 22:07:38 -0500

    added resume page, list of proficiencies and link to resume

--- src/components/tabs/Resume.js
+++ src/components/tabs/Resume.js
@@ -1,7 +1,63 @@
 import React from 'react';
+import resume from '../../resume.pdf'
 
 function Resume() {
-
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='section center'>
+          <h3>Proficiencies</h3>
+          <div className='col s4'>
+          <ul>
+            <li>Front-End Development</li>
+            <li>Terminal</li>
+            <li>Git</li>
+            <li>Git Bash</li>
+            <li>GitHub</li>
+            <li>Heroku</li>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>Flexbox</li>
+            <li>C++</li>
+            <li>Python</li>
+          </ul>
+          </div>
+          <div className='col s4'>
+          <ul>
+            <li>JavaScript/JS</li>
+            <li>JQuery</li>
+            <li>Bootstrap</li>
+            <li>Node.js</li>
+            <li>npm</li>
+            <li>JSON</li>
+            <li>ECMAScript6 / ES6</li>
+            <li>APIs</li>
+            <li>Unit Testing</li>
+            <li>Express</li>
+            <li>Express Handlebars</li>
+          </ul>
+          </div>
+          <div className='col s4'>
+          <ul>
+            <li>Materialize</li>
+            <li>React</li>
+            <li>MySQL</li>
+            <li>Sequelize</li>
+            <li>RESTful APIs</li>
+            <li>MongoDB</li>
+            <li>Mongoose</li>
+            <li>GraphQL</li>
+            <li>Flexbox</li>
+            <li>PWAs</li>
+            <li>MERN</li>
+          </ul>
+          </div>
+        </div>
+      </div>
+    <h4 className='center'><a href={resume} alt='link to resume' target='_blank' rel='noreferrer noopener'>Link to Resume</a>
+    </h4>
+    </div>
+  );
 }
 
 export default Resume;
