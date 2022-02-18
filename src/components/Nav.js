4cf1fc0b5e6ef5dcf8cf728b281c01c05279a23e
Tina Stanczyk <t9.stanczyk@gmail.com>
2022-02-17 19:21:29 -0500

    added functionality to switch tabs in navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 
-function Nav() {
+function Nav({ currentTab, handleTabChange }) {
   return (
     <nav>
       <div className='nav-wrapper'>
-      <a href="#!" className="brand-logo">Tina Stanczyk</a>
+      <a href="#about" className="brand-logo" onClick={() => handleTabChange('About')} >Tina Stanczyk</a>
       <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
         <ul className = 'right hide-on-med-and-down'>
-          <li><a href='#'>About</a></li>
-          <li><a href='#'>Portfolio</a></li>
-          <li><a href='#'>Resume</a></li>
-          <li><a href='#'>Contact</a></li>
+          <li><a href='#about' onClick={() => handleTabChange('About')} className={currentTab === 'About' ? 'nav-link active' : 'nav-link' } >About</a></li>
+          <li><a href='#portfolio' onClick={() => handleTabChange('Portfolio')} className={currentTab === 'Portfolio' ? 'nav-link active' : 'nav-link' } >Portfolio</a></li>
+          <li><a href='#resume' onClick={() => handleTabChange('Resume')} className={currentTab === 'Resume' ? 'nav-link active' : 'nav-link' } >Resume</a></li>
+          <li><a href='#contact' onClick={() => handleTabChange('Contact')} className={currentTab === 'Contact' ? 'nav-link active' : 'nav-link' } >Contact</a></li>
         </ul>
         <ul className='sidenav' id="mobile-demo">
         <li><a href='#'>About</a></li>
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,20 +1,23 @@
 import React, { useState } from 'react';
-import Header from './Header';
-import Headshot from './Headshot';
-import Contact from './Contact';
+import Contact from './tabs/Contact';
+import About from './tabs/About';
+import Project from './tabs/Project';
+import Resume from './tabs/Resume';
+import Nav from './Nav';
 
 
 function Portfolio() {
   
-  const [currentTab, setCurrentTab] = useState('Top');
+  const [currentTab, setCurrentTab] = useState('About');
 
   const renderTab = () => {
-    if(currentTab === 'Top'){
-      return <Top />;
-    }
 
     if(currentTab === 'About'){
       return <About />;
+    }
+
+    if(currentTab === 'Portfolio'){
+      return <Project />
     }
 
     if(currentTab === 'Resume'){
@@ -26,12 +29,11 @@ function Portfolio() {
     }
   }
 
-  handleTabChange = (tab) => setCurrentTab(tab);
+  const handleTabChange = (tab) => setCurrentTab(tab);
+
   return (
     <div>
-      <Header />
-      <Headshot />
-      <Contact />
+      <Nav currentTab={currentTab} handleTabChange={handleTabChange} />
     </div>
   );
 }
