53706871576ec2a227fb2ca3438e56e283e532ba
Tina Stanczyk <t9.stanczyk@gmail.com>
2022-03-17 10:32:54 -0400

    added email and styled

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,8 +5,8 @@ function Footer() {
   return(
     <div className='container'>
       <div className='row s12 center contact-links'>
-        <a href="https://github.com/tinastanczyk" alt='link to github' target='_blank'>Github</a>
-        <a href="https://www.linkedin.com/in/christina-stanczyk-a9a266222/" alt='link to linkedIn' target='_blank'>LinkedIn</a>
+        <a href="https://github.com/tinastanczyk" alt='link to github' target='_blank' rel="noreferrer">Github</a>
+        <a href="https://www.linkedin.com/in/christina-stanczyk-a9a266222/" alt='link to linkedIn' target='_blank' rel="noreferrer">LinkedIn</a>
         <p>Call: 856-254-3820</p>
         <p>Email: t9.stanczyk@gmail.com</p>
       </div>
